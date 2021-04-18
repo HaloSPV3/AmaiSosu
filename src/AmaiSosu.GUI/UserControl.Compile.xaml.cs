63be3ddf813a8f0e353ffa4829781a371765a91a
Noah Sherwin <noahrsherwin@gmail.com>
2021-04-18 00:25:40 -0700

    Start designing the Compile UserControl

--- src/AmaiSosu.GUI/UserControl.Compile.xaml.cs
+++ src/AmaiSosu.GUI/UserControl.Compile.xaml.cs
@@ -1,17 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Microsoft.Win32;
+using System;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace AmaiSosu.GUI
 {
@@ -20,9 +11,62 @@ namespace AmaiSosu.GUI
     /// </summary>
     public partial class UserControlCompile : UserControl
     {
+        /// <summary>
+        /// An instance of the Main class pass to this class.
+        /// </summary>
+        private Main _main = null;
+
         public UserControlCompile()
         {
             InitializeComponent();
+            if (!_main.Equals(null))
+                return;
+            else
+            {
+                var msg = $"{Name} was started without an instanced DataContext of {_main.GetType().Name}.";
+                throw new NullReferenceException(msg);
+            }
+        }
+
+        public UserControlCompile(Main main = null)
+        {
+            
+            InitializeComponent();
+            if (!_main.Equals(null))
+                _main = main;
+            else
+            {
+                var msg = $"{Name} was started without an instanced DataContext of {_main.GetType().Name}.";
+                throw new NullReferenceException(msg);
+            }
+        }
+
+        public Main Main
+        {
+            set
+            {
+                _main = value;
+            }
+        }
+
+        private async void Compile(object sender, RoutedEventArgs e)
+        {
+            CompileButton.IsEnabled = false;
+
+            await Task.Run(() => _main.Compile.Invoke());
+
+            CompileButton.IsEnabled = true;
+        }
+
+        private void Browse(object sender, RoutedEventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog
+            {
+                Filter = "HCE Executable|haloce.exe"
+            };
+
+            if (openFileDialog.ShowDialog() == true)
+                _main.Path = System.IO.Path.GetDirectoryName(openFileDialog.FileName);
         }
     }
 }
