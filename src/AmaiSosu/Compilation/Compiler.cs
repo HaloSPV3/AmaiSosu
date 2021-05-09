bfbba470dd8b7be669fca65cd0a948fb0cd3e1bf
Noah Sherwin <noahrsherwin@gmail.com>
2021-05-09 16:25:57 -0700

    feat(Compile): Improve file/directory verification for prepping for Compilation feat(Compile): Create Compile() function, mirroring Installer's Install() refactor(Compile): Remodel Compiler class to mirror Installer class

--- src/AmaiSosu/Compilation/Compiler.cs
+++ src/AmaiSosu/Compilation/Compiler.cs
@@ -8,10 +8,11 @@ namespace AmaiSosu.Compilation
 {
     public class Compiler : Module
     {
+        private static string _binariesPath = string.Empty;
+        private static readonly string _progData = GetFolderPath(SpecialFolder.CommonApplicationData);
+        private static readonly string _kStudios = Path.Combine(_progData, "Kornner Studios");
         public const string LibPackage = "lib";
         public const string GuiPackage = "gui";
-        private static string _binariesPath = string.Empty;
-        private static readonly string _progDataPath = Path.Combine(GetFolderPath(SpecialFolder.CommonApplicationData), "Kornner Studios");
         public List<Package> _packages;
 
         public Compiler(string binariesPath, List<Package> packages)
@@ -26,20 +27,113 @@ namespace AmaiSosu.Compilation
             _packages = packages;
         }
 
-        public List<Package> Packages
+        /// <inheritdoc />
+        /// <returns>
+        ///     False if:
+        ///     Invalid OpenSauce binaries directory path.
+        ///     - or -
+        ///     Important OpenSauce binaries don't exist.
+        ///     - or -
+        ///     Kornner Studios or its contents do not exist.
+        ///     - or -
+        ///     Direct3D9 Extensions DXSetup is not present.
+        /// </returns>
+        public Verification Verify()
         {
-            get => _packages;
-            set
+            var dxrDir = Path.Combine(_kStudios, "OpenSauce", "dxredist");
+            var dxrFiles = new List<string>
             {
-                if (value.SequenceEqual(_packages)) return;
-                _packages = value;
+                "dsetup.dll",
+                "dsetup32.dll",
+                "dxdllreg_x86.cab",
+                "dxredist.zip",
+                "dxsetup.exe",
+                "dxupdate.cab",
+                "Jun2010_d3dx9_43_x86.cab"
+            };
+            var osMainClientBinaries = new List<string>
+            {
+                /* variants of client dll */
+                "dinput8.dll",
+                "opensauce.dll", // another app will need to move or rename this
+                Path.Combine("mods", "opensauce.dll") // needs a mod loader
+            };
+            var osMainBinaries = new List<string>
+            {
+                "CheApe.map",
+                "CheApeDLLG.dll",
+                "CheApeDLLS.dll",
+                "CheApeDLLT.dll",
+                "crashrpt_lang.ini",
+                "CrashRpt1401.dll",
+                "CrashSender1401.exe",
+                "dbghelp.dll",
+                "Halo1_CE_Readme.txt",
+                "Halo1_CheApe_Readme.txt",
+                "msvcp100.dll",
+                "msvcp120.dll",
+                "msvcr100.dll",
+                "msvcr120.dll",
+                "OpenSauceDedi.dll",
+                "OS_Guerilla.exe",
+                "OS_haloceded.exe",
+                "OS_Sapien.exe",
+                "OS_Settings.Editor.xml",
+                "OS_Tool.exe",
+                "vccorlib120.dll"
+            };
+
+            if (!Directory.Exists(_binariesPath))
+                return new Verification(false, "Selected directory for OpenSauce binaries doesn't exist.");
+
+            if (!osMainClientBinaries.Any(bin => File.Exists(Path.Combine(_binariesPath, bin))))
+                return new Verification(false, "OpenSauce client binary not found.");
+            foreach (var bin in osMainBinaries)
+            {
+                if (!File.Exists(Path.Combine(_binariesPath, bin)))
+                    return new Verification(false, $"OpenSauce main binary not found: {bin}");
             }
+
+            if (!Directory.Exists(_kStudios))
+                return new Verification(false, $"Kornner Studios not found in \"{_progData}\"");
+
+            if (!Directory.Exists(dxrDir))
+                return new Verification(false, $"Direct3D9 Extensions not found in {dxrDir}");
+            foreach (var dxredist in dxrFiles)
+            {
+                if (!File.Exists(Path.Combine(dxrDir, dxredist)))
+                    return new Verification(false, $"DirectX Redist file not found: {dxredist}");
+            }
+
+            if (!Directory.Exists(Path.Combine(_kStudios, "OpenSauce", "OpenSauceIDE")))
+                return new Verification(false, "OpenSauceIDE not found in '/Kornner Studios/OpenSauce/'" + NewLine
+                                                + "Deleted by Install procedure?");
+
+            return new Verification(true);
         }
 
-        // create the packages. Pass the paths. For ProgDatam, grab the folder name (in Package class).
-        public Package LibPak = new Package("lib", "", _progDataPath);
-        public Package GuiPak = new Package("gui", "", _binariesPath);
+        /// <summary>
+        ///     Compiles the OpenSauce libraries to packages.
+        /// </summary>
+        /// <exception cref="OpenSauceException">
+        ///     Invalid
+        /// </exception>
+        public void Compile()
+        {
+            /// TODO need an Output instance from a Factory class.
+            /// Summary
+            /// 1. 
+            _packages = new List<Package>{
+                new Package("lib", "", _kStudios, output: null),
+                new Package("gui", "", _binariesPath, output: null)
+            };
 
+            
+            foreach(var package in _packages)
+            {
+                package.Compile();
+            }
+        }
 
         protected override string Identifier { get; } = "OpenSauce.Compile";
     }
