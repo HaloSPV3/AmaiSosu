5922709e871db1b69cb8b2b2e52fd510a073e393
Noah Sherwin <noahrsherwin@gmail.com>
2021-05-10 21:57:53 -0700

    refactor: Add static paths class to AmaiSosu.Common

---
+++ src/AmaiSosu/Common/Paths.cs
@@ -0,0 +1,13 @@
+﻿using static System.Environment;
+using static System.IO.Path;
+using static AmaiSosu.Resources.FileNames;
+
+namespace AmaiSosu.Common
+{
+    public static class Paths
+    {
+        public static readonly string ProgData = GetFolderPath(SpecialFolder.CommonApplicationData);
+        public static readonly string KStudios = Combine(ProgData, OpenSauceDeveloper);
+        public static readonly string Temp = GetTempPath();
+    }
+}
