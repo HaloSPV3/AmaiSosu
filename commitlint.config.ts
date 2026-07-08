3b4759bf6c8521425f3c9fa7f4522feee336f6d1
Noah Sherwin <noahrsherwin@gmail.com>
2026-07-07 21:52:47 -0700

    chore(commitlint): add scope "Install"

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -13,6 +13,7 @@ const scopes = {
     + 'but are not required at runtime nor bundled with or statically linked into the published '
     + 'binaries or packages. For NuGet packages/PackageReferences, this would be anything with'
     + 'PrivateAssets="All" and no "runtime", "native", or "contentfiles" to be included in output.',
+  Install: 'Affects "src/AmaiSosu.GUI/Install.cs" or "src/AmaiSosu/Main.Install.cs',
   README: 'Affects README.md or any other README documents.',
   release: 'Reserved for release commits.',
   TODO: 'Affects TODO.md or any todo comments.',
