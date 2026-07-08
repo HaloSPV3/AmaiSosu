813c2a9b9e88573556ec38986be58929a6ce159f
Noah Sherwin <noahrsherwin@gmail.com>
2026-07-07 23:12:46 -0700

    refactor(AmaiSosu.GUI, Help): add nullable annotations

--- src/AmaiSosu.GUI/UserControl.Help.xaml.cs
+++ src/AmaiSosu.GUI/UserControl.Help.xaml.cs
@@ -7,7 +7,7 @@ namespace AmaiSosu.GUI
     /// </summary>
     public partial class UserControlHelp : UserControl
     {
-        public Help Help { get; set; }
+        public Help? Help { get; set; }
 
         public UserControlHelp()
         {
