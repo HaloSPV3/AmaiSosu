ce6cb3bb86f9b3568af31e21206e0d43af6e028b
Noah Sherwin <noahrsherwin@gmail.com>
2026-07-07 23:10:32 -0700

    refactor(AmaiSosu.GUI, Install): add nullable annotations, `MessageBox`es for if methods are called before `Compile` is assigned

--- src/AmaiSosu.GUI/UserControl.Install.xaml.cs
+++ src/AmaiSosu.GUI/UserControl.Install.xaml.cs
@@ -1,8 +1,8 @@
-﻿using Microsoft.Win32;
-using System.IO;
+﻿using System.IO;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using Microsoft.Win32;
 
 namespace AmaiSosu.GUI
 {
@@ -12,7 +12,7 @@ namespace AmaiSosu.GUI
     /// </summary>
     public partial class UserControlInstall : UserControl
     {
-        public Install Install { get; set; }
+        public Install? Install { get; set; }
 
         public UserControlInstall()
         {
@@ -24,20 +24,36 @@ namespace AmaiSosu.GUI
         {
             InstallButton.IsEnabled = false;
 
-            await Task.Run(() => Install.Invoke());
+            if (Install == null)
+            {
+                MessageBox.Show($"If you're seeing this, a programmer messed up. {nameof(UserControlInstall)}.{nameof(Install)} is null. So, {nameof(Install)} cannot be invoked.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            await Task.Run(() => Install?.Invoke());
 
             InstallButton.IsEnabled = true;
         }
 
         private void Browse(object sender, RoutedEventArgs e)
         {
+            if (Install == null)
+            {
+                MessageBox.Show($"If you're seeing this, a programmer messed up. {nameof(UserControlInstall)}.{nameof(Install)} is null. So, {nameof(Install)} cannot process paths.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
             var openFileDialog = new OpenFileDialog
             {
                 Filter = "HCE Executable|haloce.exe"
             };
 
             if (openFileDialog.ShowDialog() == true)
-                Install.Path = Path.GetDirectoryName(openFileDialog.FileName);
+            {
+                Install.Path = Path.GetDirectoryName(openFileDialog.FileName)
+                    ?? Path.GetPathRoot(openFileDialog.FileName)
+                    ?? string.Empty;
+            }
         }
     }
 }
